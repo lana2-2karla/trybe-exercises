47bd4636de1c6530b39f719d4c9ce8a185c1246a
Allana Karla <allanakarla1702@gmail.com>
2021-11-05 18:39:13 -0300

    exercicio 1

---
+++ fundamentos_web/bloco4_intro_js_e_logica_de_programacao/4-1_js_primeiros_passos/exercicios.js
@@ -0,0 +1,26 @@
+//1-Faça programas para:
+
+//Adição (a + b)
+let prim = "a";
+let seg = "b";
+console.log(prim + seg);
+
+//Subtração (a - b)
+prim = 10;
+seg = 20;
+console.log(prim - seg);
+
+//Multiplicação (a * b)
+let result = prim * seg;
+console.log(result);
+
+//Divisão (a / b)
+prim = 100;
+seg = 200;
+console.log(prim / seg);
+
+//Módulo (a % b)
+prim = 2;
+if (prim % seg) {
+    console.log(true);
+}
