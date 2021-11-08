5f4fa80c4afa2f052e6d82259c52689f9cc7d871
Allana Karla <allanakarla1702@gmail.com>
2021-11-08 19:17:02 -0300

    Exercicio 1 e 2

---
+++ fundamentos_web/bloco4_intro_js_e_logica_de_programacao/4-2_array_e_for/exercícios.js
@@ -0,0 +1,19 @@
+//1-Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
+
+// 2-Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+result = 0;
+for (let i = 0; i < numbers.length; i =+ 1) {
+    result += numbers[i];
+}
+
+console.log(result);
+
+// 3-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+
+// 4-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// 5-Utilizando for , descubra qual o maior valor contido no array e imprima-o;
