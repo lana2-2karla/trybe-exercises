93b7bf62256e7ce639670327470df394a218d684
Allana Karla <allanakarla1702@gmail.com>
2021-11-10 18:27:46 -0300

    exercicio 3

--- fundamentos_web/bloco4_intro_js_e_logica_de_programacao/4-2_array_e_for/exercícios.js
+++ fundamentos_web/bloco4_intro_js_e_logica_de_programacao/4-2_array_e_for/exercícios.js
@@ -12,6 +12,15 @@ console.log(result);
 
 // 3-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 //A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let result = 0;
+
+for(i = 0; i < numbers.length; i += 1) {
+    soma += numbers[i];
+}
+result = soma / numbers.length;
+console.log(result);
 
 
 // 4-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
