1d2536fd42e7c20d649dbee0ffab8b74ff2e6d46
Allana Karla <allanakarla1702@gmail.com>
2022-01-11 19:17:04 -0300

    Exercicios

---
+++ fundamentos_web/bloco9_testes_assincronos/callback_01.js
@@ -0,0 +1,58 @@
+// 2 - Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+//R: A, C, B;
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+
+// 3 - A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet , de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+
+const getPlanet = () => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    setTimeout(() => console.log("Returned planet: ", mars), 4_000);
+  };
+  
+  // Parte da 4 - Testando Math.random e Math.floor:
+
+  /* Math.random retorna número aleatório ente 0 e 1;
+  Número é multiplicado por 5000
+  Math.floor transforma o decimal em um número inteiro */
+
+  const messageDelay = () => Math.floor(Math.random() * 5000);
+  console.log(messageDelay());
+
+  
